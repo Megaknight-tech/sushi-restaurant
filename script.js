8322b54433ac2f73db069f36d34c9c534ae0085c
Denus <simpleoldbluestar74@gmail.com>
2025-10-19 22:36:09 +0300

    navigator

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+let button = document.querySelector(".navigator")
+let contaimer = document.querySelector(".info")
 
 
+button.addEventListener("click", function(){
+    button.innerHTML = "test"
+})
