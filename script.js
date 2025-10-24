67e2704470f3dcbd414c532122c4b58b330fd61e
Denus <simpleoldbluestar74@gmail.com>
2025-10-24 21:56:08 +0300

    nav-system

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let button = document.querySelector(".navigator")
 let contaimer = document.querySelector(".info")
+let toggle = document.querySelector(".toggle")
 
 
-button.addEventListener("click", function(){
-    button.innerHTML = "test"
-})
+
+
+
